184b242517c5fe1c95cab3a47c25d6a69f21d6a7
Mikael Berglund <mberglun@student.hive.fi>
2019-12-05 17:54:55 +0200

    day 2 part 2 finished

--- day2/intcode.c
+++ day2/intcode.c
@@ -10,28 +10,24 @@
 
 #include "intcode.h"
 
-void	print_code(int len, int *intcode)
-{
-	int i;
-
-	printf("%d", intcode[0]);
-	i = 1;
-	while (i < len)
-		printf(", %d", intcode[i++]);
-	printf("\n");
-}
-
 void	exec_intcode(int len, int *intcode)
 {
 	int		i;
+	int		opcode;
+	int		arg1, arg2, arg3;
 
 	i = 0;
 	while (intcode[i] != 99 && i < len)
 	{
-		if (intcode[i] == 1)
-			intcode[ intcode[i + 3] ] = intcode[ intcode[i + 1] ] + intcode[ intcode[i + 2] ];
-		else if (intcode[i] == 2)
-			intcode[ intcode[i + 3] ] = intcode[ intcode[i + 1] ] * intcode[ intcode[i + 2] ];
+		opcode = intcode[i];
+		arg1 = intcode[i + 1];
+		arg2 = intcode[i + 2];
+		arg3 = intcode[i + 3];
+
+		if (opcode == 1)
+			intcode[arg3] = (intcode[arg1] + intcode[arg2]);
+		else if (opcode == 2)
+			intcode[arg3] = (intcode[arg1] * intcode[arg2]);
 		else
 			break;
 		i += 4;
@@ -42,6 +38,7 @@ int		main(int argc, char **argv)
 {
 	int len;
 	int	*intcode;
+	int	*intcode_cpy;
 
 	if (argc < 2)
 		return (0);
@@ -50,9 +47,37 @@ int		main(int argc, char **argv)
 	if ((len = readcode(argv[1], &intcode)) < 1)
 		return (0);
 
-	// execute code
-	exec_intcode(len, intcode);
-	print_code(len, intcode);
+	// reset to error state 1202
+	intcode[1] = 12;
+	intcode[2] = 2;
 
+	if (!(intcode_cpy = (int*)malloc(sizeof(int) * len)))
+		return (0);
+
+	// execute code
+	memcpy(intcode_cpy, intcode, sizeof(int) * len);
+	exec_intcode(len, intcode_cpy);
+	printf("Part 1: %d\n", intcode_cpy[0]);
+
+	int ix = 0;
+	int jx = 0;
+	while (ix < 99)
+	{
+		jx = 0;
+		while (jx < 99)
+		{
+			memcpy(intcode_cpy, intcode, sizeof(int) * len);
+			intcode_cpy[1] = ix;
+			intcode_cpy[2] = jx;
+			exec_intcode(len, intcode_cpy);
+			if (intcode_cpy[0] == 19690720)
+			{
+				printf("Part 2: %d\n", 100 * ix + jx);
+				break ;
+			}
+			++jx;
+		}
+		++ix;
+	}
 	return (0);
 }
