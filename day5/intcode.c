f8b961e7c1109773d52e0eac1e7dc1ab8212c3a7
mickberg <mikael.berglund2@gmail.com>
2019-12-05 20:15:35 +0200

    move from index reference to pointer in code exec

--- day5/intcode.c
+++ day5/intcode.c
@@ -25,12 +25,12 @@ void	print_code(int len, int *intcode)
 // Get pointer to parameter based on mode
 // mode 1: parameter points to own value
 // mode 0: parameter points to index of intcode[pos]'s value
-int		*get_parameter(int *intcode, int pos, int mode)
+int		*get_parameter(int *intcode, int *pos, int mode)
 {
 	if (mode == 1)
-		return (intcode + pos);
+		return (pos);
 	else if (mode == 0)
-		return (&(intcode[ intcode[pos] ]));
+		return (&(intcode[ *pos ]));
 	else
 		return (NULL);
 }
@@ -38,23 +38,23 @@ int		*get_parameter(int *intcode, int pos, int mode)
 // Read and execute intcode
 void	exec_intcode(int *intcode)
 {
-	int		i;
 	int		opcode;
 	int		*arg1, *arg2, *respointer;
 	int		*input;
+	int		*instrp;
 
-	i = 0;
-	while (intcode[i] != 99)
+	instrp = intcode;
+	while (*instrp != 99)
 	{
 		// opcode is first 2 digits 
-		opcode = intcode[i] % 100;
+		opcode = *instrp % 100;
 
 		// Get parameter for arguments
-		arg1 = get_parameter(intcode, i + 1, intcode[i] / 100 % 10);
-		arg2 = get_parameter(intcode, i + 2, intcode[i] / 1000 % 10);
+		arg1 = get_parameter(intcode, instrp + 1, *instrp / 100 % 10);
+		arg2 = get_parameter(intcode, instrp + 2, *instrp / 1000 % 10);
 		// res pointer (where the result will be stored)
 		// SHOULD always be in position mode
-		respointer = get_parameter(intcode, i + 3, intcode[i] / 10000 % 10);
+		respointer = get_parameter(intcode, instrp + 3, *instrp / 10000 % 10);
 
 		// opcode 1: add arg1 and arg2 and put sum in respointer's address
 		if (opcode == 1)
@@ -68,24 +68,24 @@ void	exec_intcode(int *intcode)
 			if (!readcode(NULL, &input))
 				exit(0);
 			*arg1 = input[0];
-			i += 2;
+			instrp += 2;
 			continue ;;
 		}
 		// output value at arg1
 		else if (opcode == 4)
 		{
 			printf(">%d\n", *arg1);
-			i += 2;
+			instrp += 2;
 			continue ;
 		}
 		else 
 		{
-			printf("Something wrong with the code??\n");
+			printf("Something wrong with the code?? opcode: %d\n", opcode);
 			break;
 		}
 		
 		// each block of code is 4 indexes long (except input and output which are 2 long)
-		i += 4;
+		instrp += 4;
 	}
 }
 
@@ -103,7 +103,8 @@ int		main(int argc, char **argv)
 
 	// execute code
 	exec_intcode(intcode);
-
-	//print_code(len, intcode);
+	
+	// print executed code for testing
+	print_code(len, intcode);
 	return (0);
 }
